7030877db49d8efd10ce31d532f0b88692653f48
tlsrhksgh <sin99145@naver.com>
2023-07-10 18:42:53 +0900

    @target, @within 사용 예시

--- src/main/java/hello/aop/member/MemberServiceImpl.java
+++ src/main/java/hello/aop/member/MemberServiceImpl.java
@@ -1,7 +1,7 @@
 package hello.aop.member;
 
-import hello.aop.member.anotation.ClassAop;
-import hello.aop.member.anotation.MethodAop;
+import hello.aop.member.annotation.ClassAop;
+import hello.aop.member.annotation.MethodAop;
 import org.springframework.stereotype.Component;
 
 @ClassAop
--- src/main/java/hello/aop/member/annotation/ClassAop.java
+++ src/main/java/hello/aop/member/annotation/ClassAop.java
@@ -1,4 +1,4 @@
-package hello.aop.member.anotation;
+package hello.aop.member.annotation;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
